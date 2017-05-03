37332a3a7f7870e0ff7df57bfb9c20ccf1c58752
Mohammad Hossein Sedighi Gilani <mohammadhosseinsedighigilani@MHSGs-MacBook-Pro.local>
2017-05-02 21:24:24 -0400

    Finished the first plot

--- Visualization/learn_pandas.py
+++ Visualization/learn_pandas.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('data/unrate.csv')
-print(df['DATE'])
+unrate = pd.read_csv('data/unrate.csv')
+unrate['DATE'] = pd.to_datetime(unrate['DATE'])
+x_values = unrate.head(12)['DATE']
+y_values = unrate.head(12)['VALUE']
+
+plt.plot(x_values, y_values)
+plt.xticks(rotation=90)
+plt.xlabel('Month')
+plt.ylabel('Unemployment Rate')
+plt.title("Monthly Unemployment Trends, 1948")
+plt.show()
