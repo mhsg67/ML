3e1afa688d84a7c3b561e4193e80a18b5bf1e95c
Mehrdad_Niasari <Arabpour.m@gmail.com>
2017-06-26 19:45:34 -0400

    Cleaned up.

--- Kaggle/DigitRecognizer/solution.py
+++ Kaggle/DigitRecognizer/solution.py
@@ -1,34 +1,34 @@
 import pandas as ps
 from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 
+def ShowDigit(digit):
+    im = digit
+    im = np.reshape(im, (28, 28), order="F")  # "F" means read/write by 1st index changing fastest, last index slowest.
+    plt.imshow(im, cmap='gray')
+    plt.plot()
 
+def predict(classifier, X, expectedAns=None):
+    clf = classifier #goshadi
+    if (expectedAns!=None):
+        ShowDigit(expectedAns)
+        print("expected Answer is: " + expectedAns)
 
+    print("predict proba: " + clf.predict_proba([X]))
+    print("predict: " + clf.predict([X]))
 
 digits = ps.read_csv('data/train.csv')
 X = digits.drop(['label'], axis = 1)
 Y = digits['label']
 
 
-#clf = MLPClassifier(solver='sgd', alpha=1e-5, hidden_layer_sizes=(5,2), random_state=1)
-#clf.fit(X, Y)
-#print(clf.classes_)
+sgd_clf = MLPClassifier(solver='sgd', hidden_layer_sizes=(30,2), random_state=1)
+sgd_clf.fit(X, Y)
 
 
-im = X.loc[1]  # picked a row at random
-im = np.reshape(im, (28, 28), order="F")  #"F" means read/write by 1st index changing fastest, last index slowest.
-plt.imshow(im,cmap='gray')
-plt.plot()
+predict(sgd_clf, X.iloc[2], Y.iloc[2])
 
 
-#print(clf.predict_proba([X.iloc[1]]))
-#print(Y.loc[1])
-#print
-#print(clf.predict_proba(X.iloc[2]))
-#print(Y.loc[2])
-#print
-#print(clf.predict_proba([X.iloc[3]]))
-#print(Y.loc[3])
+
 
