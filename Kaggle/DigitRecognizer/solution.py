32831104909d2c1a93765dc57621968023d87117
Mehrdad_Niasari <Arabpour.m@gmail.com>
2017-06-26 21:09:43 -0400

    created 100 layers with sgd, 94% accuracy.

--- Kaggle/DigitRecognizer/solution.py
+++ Kaggle/DigitRecognizer/solution.py
@@ -2,6 +2,8 @@ import pandas as ps
 from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.externals import joblib
+import os
 
 def showDigit(digit):
     im = digit
@@ -10,26 +12,39 @@ def showDigit(digit):
     plt.plot()
     plt.show()
 
-def predict(classifier, X, expectedAns=None):
-    showDigit(X)
+def predict(classifier, X, expectedAns=None,toPrint=False):
+    #showDigit(X)
     clf = classifier #goshadi
-    if (expectedAns!=None):
+    if (expectedAns!=None and toPrint):
         print("expected Answer is: " + str(expectedAns))
 
-    print("predict proba: " + str(clf.predict_proba([X])))
-    print("predict: " + str(clf.predict([X])))
+    result = clf.predict([X])[0]
+    if (toPrint):
+        print("predict proba: " + str(clf.predict_proba([X])))
+        print("predict: " + str(result))
+
+    if (result == expectedAns):
+        return 1
+    else:
+        return 0
 
 digits = ps.read_csv('data/train.csv')
 X = digits.drop(['label'], axis = 1)
 Y = digits['label']
 
+clf = None
+if os.path.isfile("model_100.pkl"):
+    clf = joblib.load("model_100.pkl")
+else:
+    sgd_clf = MLPClassifier(solver='sgd', hidden_layer_sizes=(100,), random_state=1)
+    sgd_clf.fit(X, Y)
+    clf = sgd_clf
+    joblib.dump(clf,"model_100.pkl")
 
-sgd_clf = MLPClassifier(solver='sgd', hidden_layer_sizes=(30,2), random_state=1)
-sgd_clf.fit(X, Y)
-
-
-predict(sgd_clf, X.iloc[2], Y.iloc[2])
-
-
+tp = 0
+size = digits.__len__()
+for i in range(0,size):
+    tp = tp + (int)(predict(clf,X.iloc[i],Y.iloc[i]))
+print((tp)/(size*1.))
 
 
